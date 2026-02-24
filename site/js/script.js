f64d2cbafc563b331685a922a24c05218a427516
Felix <jimontd@icloud.com>
2026-02-25 00:33:41 +0900

    update

--- site/js/script.js
+++ site/js/script.js
@@ -13,9 +13,9 @@ const bgImages = [
 ];
 
 const VIDEO_SRC = "video/skull-edit1.2.mp4";
-const VIDEO_INTERVAL_MINUTES = 5;
+const VIDEO_INTERVAL_MINUTES = 1;
 const VIDEO_LOOPS = 10;
-const VIDEO_VOLUME = 0.35;
+const VIDEO_VOLUME = 0.5;
 
 let bgImageEl = null;
 let bgVideoEl = null;
